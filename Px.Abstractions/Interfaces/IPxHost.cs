cff4a4099eaccdd0211cebc3ad6d7adf51c06546
Johannes <jfi@ssb.no>
2024-02-15 15:02:40 +0100

    Added GlobalUsings.cs for Px.Abstractions

--- Px.Abstractions/Interfaces/IPxHost.cs
+++ Px.Abstractions/Interfaces/IPxHost.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Px.Abstractions.Interfaces
+﻿namespace Px.Abstractions.Interfaces
 {
     public interface IPxHost
     {
