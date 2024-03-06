e8bd7ba8bd5cb1a1804ac8f9c445e560d98f8c85
Johannes <jfi@ssb.no>
2024-03-06 09:41:29 +0100

    Adding test that fails :-)

---
+++ PxWebApi.BigTests/LabTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Routing.Constraints;
+using Microsoft.Extensions.Hosting.Internal;
+using Microsoft.Extensions.Options;
+using PxWeb.Code.BackgroundWorker;
+using PxWeb.Controllers.Api2.Admin;
+using System;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PxWebApi.BigTests
+{
+    [TestClass]
+    public class LabTest
+    {
+        [TestMethod]
+
+
+        // TODO set Thread.CurrentThread.CurrentCulture  in all methods
+
+        public void Raw()
+        {
+            var raw = DateTime.Now.ToString();
+            Assert.AreEqual("raw,"+ CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern, raw);
+        }
+
+        [TestMethod]
+        public void withInvariantCulture()
+        {
+            // Clone the InvariantCulture
+            CultureInfo customCulture = (CultureInfo)CultureInfo.InvariantCulture.Clone();
+
+            // Set the desired date and time pattern
+            customCulture.DateTimeFormat.ShortDatePattern = "yyyy_MM_dd";
+            customCulture.DateTimeFormat.LongDatePattern = "yyyy_MM_dd";
+            customCulture.DateTimeFormat.ShortTimePattern = "HH:mm";
+            customCulture.DateTimeFormat.LongTimePattern = "HH:mm";
+
+            // Apply the custom culture to the current thread
+            Thread.CurrentThread.CurrentCulture = customCulture;
+            Thread.CurrentThread.CurrentUICulture = customCulture;
+
+            var withIC = DateTime.Now.ToString();
+            Assert.AreEqual("withIC ," + CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern, withIC);
+        }
+
+    }
+}
