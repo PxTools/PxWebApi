cf8fff147afb7228bb2ea497953cc44f481f26c4
Johannes <jfi@ssb.no>
2024-03-06 14:39:27 +0100

    Will this also pass on Linux again?

--- PxWebApi.BigTests/LabTest.cs
+++ PxWebApi.BigTests/LabTest.cs
@@ -16,8 +16,21 @@ namespace PxWebApi.BigTests
         [TestMethod]
         public void DatetimeToStringWithInvariantCulture()
         {
+            var aDatetime = new DateTime(2020, 12, 24, 16, 59, 59);
+
+            var customCulture = (CultureInfo)CultureInfo.CurrentCulture.Clone();
+            customCulture.DateTimeFormat.ShortDatePattern = "yyyy-MM-dd";
+            customCulture.DateTimeFormat.LongDatePattern = "yyyy-MM-dd";
+            customCulture.DateTimeFormat.ShortTimePattern = "HH:mm:ss";
+            customCulture.DateTimeFormat.LongTimePattern = "HH:mm:ss";
+
+            Thread.CurrentThread.CurrentCulture = customCulture;
+
+            var withCC = aDatetime.ToString();
+            Assert.AreEqual("2020-12-24 16:59:59", withCC);
+
             // Clone the InvariantCulture
-            CultureInfo customCulture = (CultureInfo)CultureInfo.InvariantCulture.Clone();
+            customCulture = (CultureInfo)CultureInfo.InvariantCulture.Clone();
 
             // Set the desired date and time pattern
             customCulture.DateTimeFormat.ShortDatePattern = "yyyy-MM-dd";
@@ -29,7 +42,7 @@ namespace PxWebApi.BigTests
             Thread.CurrentThread.CurrentCulture = customCulture;
             Thread.CurrentThread.CurrentUICulture = customCulture;
 
-            var aDatetime = new DateTime(2020, 12, 24, 16, 59, 59);
+           
 
             var withIC = aDatetime.ToString();
             Assert.AreEqual("2020-12-24 16:59:59", withIC);
