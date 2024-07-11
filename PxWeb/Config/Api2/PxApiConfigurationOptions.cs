1c49b257b72e235bf30fc8d2a79e5798be76b779
Rune Johansen <6797636+runejo@users.noreply.github.com>
2024-07-11 15:17:38 +0200

    Add appVersion to config endpoint

--- PxWeb/Config/Api2/PxApiConfigurationOptions.cs
+++ PxWeb/Config/Api2/PxApiConfigurationOptions.cs
@@ -7,15 +7,16 @@ namespace PxWeb.Config.Api2
     public class PxApiConfigurationOptions
     {
         private readonly string _apiVersion;
+        private readonly string _appVersion;
 
         public PxApiConfigurationOptions()
         {
-            Assembly api2ServerAssembly = Assembly.Load("PxWeb.Api2.Server");
-            var fileVersionInfo = System.Diagnostics.FileVersionInfo.GetVersionInfo(api2ServerAssembly.Location);
-            _apiVersion = fileVersionInfo.ProductVersion ?? "2.0.0";
+            _apiVersion = System.Diagnostics.FileVersionInfo.GetVersionInfo(Assembly.Load("PxWeb.Api2.Server").Location).ProductVersion ?? "2.0.0";
+            _appVersion = System.Diagnostics.FileVersionInfo.GetVersionInfo(Assembly.GetEntryAssembly()!.Location).ProductVersion ?? "2.0.0";
         }
 
         public string ApiVersion => _apiVersion;
+        public string AppVersion => _appVersion;
 
         public List<Language> Languages { get; set; } = new List<Language>();
         public string DefaultLanguage { get; set; } = String.Empty;
