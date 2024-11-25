b4e8d6dc4c1fdc1bb8d029fb55cab6405ae14314
Rune Johansen <6797636+runejo@users.noreply.github.com>
2024-11-25 15:01:30 +0100

    Add support for docker compose

---
+++ docker/pxweb2/public/config/config.js
@@ -0,0 +1,10 @@
+window.PxWeb2Config = {
+  language: {
+    supportedLanguages: [
+      { shorthand: "en", languageName: "English" }
+    ],
+    defaultLanguage: "en",
+    fallbackLanguage: "en",
+  },
+  apiUrl: "//localhost:8081/api/v2",
+};
---
+++ docker/pxwebapi/Database/README.md
@@ -0,0 +1,9 @@
+# Database folder
+
+To test PxWebApi with your own PX file database you must copy your database files to this folder.
+
+Some files and folders will be overwritten
+
+- Menu.xml
+- \_INDEX
+- database.config(?)
