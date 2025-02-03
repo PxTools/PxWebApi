752d35860b2f9c8baca1f1a0344ad8d607ffd39d
likp <petros.likidis@scb.se>
2025-02-03 13:45:48 +0100

    Refactored ModelBinder classes to make them easier to test

--- PxWeb/Code/Api2/ModelBinder/CommaSeparatedStringToListOfStrings.cs
+++ PxWeb/Code/Api2/ModelBinder/CommaSeparatedStringToListOfStrings.cs
@@ -1,38 +1,9 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
-
-using Microsoft.AspNetCore.Mvc.ModelBinding;
-
-namespace PxWeb.Code.Api2.ModelBinder
+﻿namespace PxWeb.Code.Api2.ModelBinder
 {
-    public class CommaSeparatedStringToListOfStrings : IModelBinder
+    public class CommaSeparatedStringToListOfStrings : GenericListModelBinder<string>
     {
-
-        public Task BindModelAsync(ModelBindingContext bindingContext)
+        public CommaSeparatedStringToListOfStrings() : base(x => x)
         {
-
-            ArgumentNullException.ThrowIfNull(bindingContext);
-
-            var modelName = bindingContext.ModelName;
-
-            var result = new List<string>();
-
-            var keys = bindingContext.HttpContext.Request.Query.Keys.Where(x => x.Equals(modelName, StringComparison.InvariantCultureIgnoreCase));
-
-            foreach (var key in keys)
-            {
-                string? q = bindingContext.HttpContext.Request.Query[key];
-                if (q != null)
-                {
-                    var list = CommaSeparatedListToListConverter.ToList<string>(q, x => x);
-                    result.AddRange(list);
-                }
-            }
-
-            bindingContext.Result = ModelBindingResult.Success(result);
-
-            return Task.CompletedTask;
         }
-
     }
 }
--- PxWeb/Code/Api2/ModelBinder/OutputFormatParamsModelBinder.cs
+++ PxWeb/Code/Api2/ModelBinder/OutputFormatParamsModelBinder.cs
@@ -1,49 +1,12 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
-
-using Microsoft.AspNetCore.Mvc.ModelBinding;
-
-using PxWeb.Api2.Server.Models;
+﻿using PxWeb.Api2.Server.Models;
 using PxWeb.Converters;
 
 namespace PxWeb.Code.Api2.ModelBinder
 {
-    public class OutputFormatParamsModelBinder : IModelBinder
+    public class OutputFormatParamsModelBinder : GenericListModelBinder<OutputFormatParamType>
     {
-
-        public Task BindModelAsync(ModelBindingContext bindingContext)
+        public OutputFormatParamsModelBinder() : base(x => EnumConverter.ToEnum<OutputFormatParamType>(x))
         {
-
-            ArgumentNullException.ThrowIfNull(bindingContext);
-
-            var modelName = bindingContext.ModelName;
-
-            var result = new List<OutputFormatParamType>();
-
-            var keys = bindingContext.HttpContext.Request.Query.Keys.Where(x => x.Equals(modelName, StringComparison.InvariantCultureIgnoreCase));
-
-            foreach (var key in keys)
-            {
-                string? q = bindingContext.HttpContext.Request.Query[key];
-                if (q != null)
-                {
-                    try
-                    {
-                        var list = CommaSeparatedListToListConverter.ToList<OutputFormatParamType>(q, x => EnumConverter.ToEnum<OutputFormatParamType>(x));
-                        result.AddRange(list);
-                    }
-                    catch (InvalidOperationException)
-                    {
-                        bindingContext.ModelState.AddModelError(bindingContext.ModelName, "Invalid value for enum.");
-                        bindingContext.Result = ModelBindingResult.Failed();
-                        return Task.CompletedTask;
-                    }
-                }
-            }
-
-            bindingContext.Result = ModelBindingResult.Success(result);
-
-            return Task.CompletedTask;
         }
     }
 }
