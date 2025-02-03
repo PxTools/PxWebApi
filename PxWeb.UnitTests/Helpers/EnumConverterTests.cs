65627068deb73aff3220db8913f141d6c69dd85a
likp <petros.likidis@scb.se>
2025-02-03 12:29:23 +0100

    Add unit tests for EnumConverter's ToEnum method
    Introduce EnumConverterTests class in PxWeb.UnitTests.Helpers.
    Add tests for OutputFormatParamType enum conversion:
    - Test valid conversion with "IncludeTitle".
    - Ensure case-insensitive conversion with "includetitle" and "INCLUDETITLE".
    - Verify InvalidOperationException for invalid value "X".

---
+++ PxWeb.UnitTests/Helpers/EnumConverterTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+using PxWeb.Converters;
+
+namespace PxWeb.UnitTests.Helpers
+{
+    [TestClass]
+    public class EnumConverterTests
+    {
+        [TestMethod]
+        public void OutputFormatParam_IncludeTile()
+        {
+            var expected = OutputFormatParamType.IncludeTitleEnum;
+            // Act
+            var actual = EnumConverter.ToEnum<OutputFormatParamType>("IncludeTitle");
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void OutputFormatParam_CaseInsensitiveLowerCase()
+        {
+            var expected = OutputFormatParamType.IncludeTitleEnum;
+            // Act
+            var actual = EnumConverter.ToEnum<OutputFormatParamType>("includetitle");
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void OutputFormatParam_CaseInsensitiveUpperCase()
+        {
+            var expected = OutputFormatParamType.IncludeTitleEnum;
+            // Act
+            var actual = EnumConverter.ToEnum<OutputFormatParamType>("INCLUDETITLE");
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void OutputFormatParam_InvalidValue_ThrowsException()
+        {
+            // Act
+            EnumConverter.ToEnum<OutputFormatParamType>("X");
+        }
+
+
+    }
+}
