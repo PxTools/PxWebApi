c54b86515e6cdc11f7144c3520a310852d0113be
likp <petros.likidis@scb.se>
2025-02-13 16:24:39 +0100

    Made it possible to have an IP-rnge in CIDR notation in the whitelist

--- PxWeb/Middleware/AdminProtectionIpWhitelistMiddleware.cs
+++ PxWeb/Middleware/AdminProtectionIpWhitelistMiddleware.cs
@@ -1,4 +1,6 @@
-﻿using System.Net;
+﻿using System.Linq;
+using System.Net;
+using System.Numerics;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Builder;
@@ -13,6 +15,7 @@ namespace PxWeb.Middleware
         private readonly AdminProtectionConfigurationOptions _adminProtectionConfigurationOptions;
         private readonly HashSet<string> _ipWhitelist = new HashSet<string>();
 
+
         public AdminProtectionIpWhitelistMiddleware(RequestDelegate next, IAdminProtectionConfigurationService adminProtectionConfigurationService)
         {
             _next = next;
@@ -24,15 +27,61 @@ namespace PxWeb.Middleware
         public async Task Invoke(HttpContext httpContext)
         {
             IPAddress? ip = httpContext.Connection.RemoteIpAddress;
-            bool match = ip != null && _ipWhitelist.Contains(ip.ToString());
+            if (ip == null) return;
+            var ipadr = ip.ToString();
+            bool match = false;
+
+            foreach (string ipw in _ipWhitelist)
+            {
+                if (ipw.Contains("/"))
+                {
+                    // CIDR
+                    match = _ipWhitelist.Any(cidr => IsInRange(ipadr, ipw));
+                }
+                else
+                {
+                    match = _ipWhitelist.Contains(ipadr);
+                }
+                if (match) break;
+            }
+
             if (!match)
             {
+
                 httpContext.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                Console.Write($"Unauthorized access from IP: {ip}");
                 return;
             }
             await _next(httpContext);
 
 
+        }
+
+        public static bool IsInRange(string ipAddress, string cidr)
+        {
+            var parts = cidr.Split('/');
+            var baseAddress = IPAddress.Parse(parts[0]);
+            var address = IPAddress.Parse(ipAddress);
+            int bits = int.Parse(parts[1]);
+
+            if (baseAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+            {
+                uint mask = ~(uint.MaxValue >> bits);
+                uint baseAddressInt = BitConverter.ToUInt32(baseAddress.GetAddressBytes(), 0);
+                uint addressInt = BitConverter.ToUInt32(address.GetAddressBytes(), 0);
+                return (baseAddressInt & mask) == (addressInt & mask);
+            }
+            else if (baseAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6)
+            {
+                BigInteger mask = BigInteger.One << (128 - bits) - 1;
+                BigInteger baseAddressInt = new BigInteger(baseAddress.GetAddressBytes());
+                BigInteger addressInt = new BigInteger(address.GetAddressBytes());
+                return (baseAddressInt & mask) == (addressInt & mask);
+            }
+            else
+            {
+                throw new ArgumentException("Invalid address family");
+            }
         }
     }
 
