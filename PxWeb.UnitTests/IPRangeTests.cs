67a25f0995c1db3c1520c497cf18deab64a1caf3
likp <petros.likidis@scb.se>
2025-02-17 12:48:48 +0100

    Added more test cases for IsInRange

--- PxWeb.UnitTests/IPRangeTests.cs
+++ PxWeb.UnitTests/IPRangeTests.cs
@@ -28,5 +28,65 @@ namespace PxWeb.UnitTests
         {
             Assert.IsTrue(!AdminProtectionIpWhitelistMiddleware.IsInRange("172.17.2.77", "192.168.0.0/16"));
         }
+
+        [TestMethod]
+        public void IPv6ExactMatch_ReturnsTrue()
+        {
+            Assert.IsTrue(AdminProtectionIpWhitelistMiddleware.IsInRange("::ffff:192.168.2.77", "::ffff:192.168.2.77/128"));
+        }
+
+        [TestMethod]
+        public void IPv6DifferentSubnet_ReturnsFalse()
+        {
+            Assert.IsTrue(!AdminProtectionIpWhitelistMiddleware.IsInRange("::ffff:192.168.2.77", "::ffff:192.169.0.0/112"));
+        }
+
+        [TestMethod]
+        public void IPv4ExactMatch_ReturnsTrue()
+        {
+            Assert.IsTrue(AdminProtectionIpWhitelistMiddleware.IsInRange("192.168.2.77", "192.168.2.77/32"));
+        }
+
+        [TestMethod]
+        public void IPv4DifferentSubnet_ReturnsFalse()
+        {
+            Assert.IsTrue(!AdminProtectionIpWhitelistMiddleware.IsInRange("192.168.2.77", "192.169.0.0/16"));
+        }
+
+        [TestMethod]
+        public void IPv6InRangeWithDifferentPrefixLength_ReturnsTrue()
+        {
+            Assert.IsTrue(AdminProtectionIpWhitelistMiddleware.IsInRange("::ffff:192.168.2.77", "::ffff:192.168.0.0/96"));
+        }
+
+        [TestMethod]
+        public void IPv4InRangeWithDifferentPrefixLength_ReturnsFalse()
+        {
+            Assert.IsTrue(!AdminProtectionIpWhitelistMiddleware.IsInRange("192.168.2.77", "192.168.0.0/24"));
+        }
+
+        [TestMethod]
+        public void IPv6LoopbackInRange_ReturnsTrue()
+        {
+            Assert.IsTrue(AdminProtectionIpWhitelistMiddleware.IsInRange("::1", "::1/128"));
+        }
+
+        [TestMethod]
+        public void IPv4LoopbackInRange_ReturnsTrue()
+        {
+            Assert.IsTrue(AdminProtectionIpWhitelistMiddleware.IsInRange("127.0.0.1", "127.0.0.1/32"));
+        }
+
+        [TestMethod]
+        public void IPv6LoopbackOutsideOfRange_ReturnsFalse()
+        {
+            Assert.IsTrue(!AdminProtectionIpWhitelistMiddleware.IsInRange("::1", "::2/128"));
+        }
+
+        [TestMethod]
+        public void IPv4LoopbackOutsideOfRange_ReturnsFalse()
+        {
+            Assert.IsTrue(!AdminProtectionIpWhitelistMiddleware.IsInRange("127.0.0.1", "127.0.0.2/32"));
+        }
     }
 }
