1905541529c07ce7fc4b7bc286f04f6a377ef2b2
likp <petros.likidis@scb.se>
2025-03-04 11:53:23 +0100

    Removed unused method

--- PxWeb/Mappers/ISelectionResponseMapper.cs
+++ PxWeb/Mappers/ISelectionResponseMapper.cs
@@ -1,13 +1,9 @@
-﻿using PCAxis.Paxiom;
-
-using PxWeb.Api2.Server.Models;
+﻿using PxWeb.Api2.Server.Models;
 
 namespace PxWeb.Mappers
 {
     public interface ISelectionResponseMapper
     {
-        SelectionResponse Map(Selection[] selections, List<string> heading, List<string> stub, PXMeta meta, string tableId, string lang);
-
         SelectionResponse Map(VariablesSelection selections, string tableId, string lang);
     }
 }
--- PxWeb/Mappers/SelectionResponseMapper.cs
+++ PxWeb/Mappers/SelectionResponseMapper.cs
@@ -1,8 +1,4 @@
-﻿using System.Linq;
-
-using PCAxis.Paxiom;
-
-using PxWeb.Api2.Server.Models;
+﻿using PxWeb.Api2.Server.Models;
 
 namespace PxWeb.Mappers
 {
@@ -30,52 +26,6 @@ namespace PxWeb.Mappers
             return response;
         }
 
-
-
-        public SelectionResponse Map(Selection[] selections, List<string> heading, List<string> stub, PXMeta meta, string tableId, string lang)
-        {
-            var response = new SelectionResponse();
-            response.Language = lang;
-            response.Selection = selections.Select(selection => new VariableSelection()
-            {
-                VariableCode = selection.VariableCode,
-                CodeList = GetCodeList(meta.Variables.FirstOrDefault(v => string.Compare(v.Code, selection.VariableCode, true) == 0)),
-                ValueCodes = selection.ValueCodes.Cast<string>().ToList(),
-            }).ToList();
-
-            response.Placement = new VariablePlacementType()
-            {
-                Heading = heading,
-                Stub = stub
-            };
-
-            response.Links = new List<Link>();
-
-            response.Links.Add(_linkCreator.GetDefaultSelectionLink(LinkCreator.LinkRelationEnum.self, tableId, lang, true));
-
-            return response;
-        }
-
-        private static string? GetCodeList(Variable? variable)
-        {
-            if (variable == null)
-            {
-                return null;
-            }
-
-            if (variable.CurrentGrouping != null)
-            {
-                return "agg_" + variable.CurrentGrouping.ID;
-            }
-
-            if (variable.CurrentValueSet != null)
-            {
-                return "vs_" + variable.CurrentValueSet.ID;
-            }
-
-            return null;
-
-        }
     }
 
 
