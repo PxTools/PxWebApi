89476780870ff9469c883912a0c8323b64ed84e9
likp <petros.likidis@scb.se>
2025-04-04 10:44:08 +0200

    More tests

--- PxWeb.UnitTests/SavedQuery/SaveQueryFileStorgeBackendTests.cs
+++ PxWeb.UnitTests/SavedQuery/SaveQueryFileStorgeBackendTests.cs
@@ -15,7 +15,7 @@ namespace PxWeb.UnitTests.SavedQuery
             var options = new Mock<IOptions<SavedQueryFileStorageOptions>>();
             options.Setup(x => x.Value).Returns(settings);
             var host = new Mock<IPxHost>();
-            host.Setup(h => h.RootPath).Returns(Path.GetTempPath);
+            host.Setup(h => h.RootPath).Returns(Path.GetTempPath());
             var backend = new SaveQueryFileStorgeBackend(options.Object, host.Object);
 
             // Act
@@ -24,5 +24,29 @@ namespace PxWeb.UnitTests.SavedQuery
             // Assert
             Assert.IsFalse(result);
         }
+
+        [TestMethod]
+        public void UpdatedRunStatistics_WhenNoFileButQuery_ReturnTrueAndCreatesFile()
+        {
+            // Arrange
+            var settings = new SavedQueryFileStorageOptions() { Path = "SQ" };
+            var options = new Mock<IOptions<SavedQueryFileStorageOptions>>();
+            options.Setup(x => x.Value).Returns(settings);
+            var host = new Mock<IPxHost>();
+            host.Setup(h => h.RootPath).Returns(Path.GetTempPath());
+            var backend = new SaveQueryFileStorgeBackend(options.Object, host.Object);
+
+            var path = Path.Combine(Path.GetTempPath(), "SQ", "i");
+            Directory.CreateDirectory(path);
+            File.CreateText(Path.Combine(path, "id.sqa")).Close();
+
+            // Act
+            var result = backend.UpdateRunStatistics("id");
+
+            // Assert
+            Assert.IsTrue(result);
+        }
     }
+
+
 }
