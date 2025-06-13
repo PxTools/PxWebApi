07df8c634ebc81da116e5e811b25a8f146d52060
Rune Johansen <runejo@gmail.com>
2025-06-13 14:00:45 +0200

    docs: update docker compose sample config

--- docker/pxweb2/public/config/config.js
+++ docker/pxweb2/public/config/config.js
@@ -1,10 +1,11 @@
 window.PxWeb2Config = {
   language: {
-    supportedLanguages: [
-      { shorthand: "en", languageName: "English" }
-    ],
+    supportedLanguages: [{ shorthand: "en", languageName: "English" }],
     defaultLanguage: "en",
     fallbackLanguage: "en",
+    showDefaultLanguageInPath: true,
   },
   apiUrl: "//localhost:8081/api/v2",
+  maxDataCells: 150000,
+  specialCharacters: [".", "..", ":", "-", "...", "*"],
 };
