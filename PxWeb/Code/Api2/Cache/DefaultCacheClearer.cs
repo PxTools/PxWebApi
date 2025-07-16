a319aea937dda7caa016176cc42f32fb3c4e7cbe
likp <petros.likidis@scb.se>
2025-07-16 13:52:49 +0200

    Enhance cache management with DefaultCacheClearer
    Introduces the `DefaultCacheClearer` class for managing cache coherence based on a specified clear time. Updates the `IPxCache` registration in `Program.cs` to use a factory method that initializes `PxCache` with a logger and sets up a coherence checker. The clear time is retrieved from configuration, improving cache management and ensuring coherence.

---
+++ PxWeb/Code/Api2/Cache/DefaultCacheClearer.cs
@@ -0,0 +1,26 @@
+﻿namespace PxWeb.Code.Api2.Cache
+{
+    public class DefaultCacheClearer
+    {
+        public static DateTime NextClearTime { get; set; } = DateTime.Now;
+
+        public DefaultCacheClearer(DateTime nextClearTime)
+        {
+            if (nextClearTime < DateTime.Now)
+            {
+                nextClearTime = nextClearTime.AddDays(1);
+            }
+            NextClearTime = nextClearTime;
+        }
+
+        public bool CacheIsCoherent()
+        {
+            if (DateTime.Now >= NextClearTime)
+            {
+                NextClearTime = DateTime.Now.AddDays(1);
+                return false;
+            }
+            return true;
+        }
+    }
+}
