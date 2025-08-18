2bff99e1c9c1e62a697c65214d4935bba41e679c
likp <petros.likidis@scb.se>
2025-08-18 12:15:09 +0200

    Update health check status and add unit tests
    Modified `CheckHealthAsync` to return `Degraded` status when a maintenance file is found. Added `MaintenanceHealthCheckTests` with tests for both healthy and degraded states, ensuring correct behavior of the health check under different conditions.

---
+++ PxWeb.UnitTests/Helpers/MaintenanceHealthCheckTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Threading;
+
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+using PxWeb.Code;
+
+namespace PxWeb.UnitTests.Helpers
+{
+
+    [TestClass]
+    public class MaintenanceHealthCheckTests
+    {
+
+        [TestMethod]
+        public void When_NoMaintenanceFile_Then_HealthCheckIsHealthy()
+        {
+            var host = new Mock<IPxHost>();
+            var tempPath = Path.GetTempPath();
+
+            host.Setup(h => h.RootPath).Returns(tempPath);
+
+            // Arrange
+            var healthCheck = new MaintenanceHealthCheck(host.Object);
+
+            // Act
+            var context = new HealthCheckContext();
+            var cancellationToken = CancellationToken.None;
+            var result = healthCheck.CheckHealthAsync(context, cancellationToken).Result;
+            // Assert
+            Assert.AreEqual(result.Status, HealthStatus.Healthy);
+        }
+
+
+        [TestMethod]
+        public void When_MaintenanceFile_Then_HealthCheckIsDegraded()
+        {
+            var host = new Mock<IPxHost>();
+            var tempPath = Path.GetTempPath();
+            var maintenanceFilePath = Path.Combine(tempPath, ".maintenance");
+
+            if (!File.Exists(maintenanceFilePath))
+            {
+                File.Create(maintenanceFilePath).Close();
+            }
+
+            host.Setup(h => h.RootPath).Returns(tempPath);
+
+            // Arrange
+            var healthCheck = new MaintenanceHealthCheck(host.Object);
+
+            // Act
+            var context = new HealthCheckContext();
+            var cancellationToken = CancellationToken.None;
+            var result = healthCheck.CheckHealthAsync(context, cancellationToken).Result;
+            // Assert
+            Assert.AreEqual(result.Status, HealthStatus.Degraded);
+
+            File.Delete(maintenanceFilePath);
+        }
+
+    }
+}
