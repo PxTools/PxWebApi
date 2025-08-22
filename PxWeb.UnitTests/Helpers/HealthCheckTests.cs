d67e1a7be8870a63708298445494543bd81a52ef
Petros Likidis <petros.likidis@scb.se>
2025-08-22 15:03:30 +0200

    feat: Added health check endpoints PXWEB2-353 (#407)
    * Add maintenance health check feature
    
    Introduce a new health check in `MaintenanceHealthCheck.cs` to verify if the application is in maintenance mode by checking for a `.maintenance` file. Update `Program.cs` to register the health check service and map endpoints (`/healthz/live` and `/healthz/ready`). Modify middleware to exclude health check routes from specific processing logic.
    
    * Add SQL database health check and update dependencies
    
    Introduces `SqlDbConnectionHealthCheck` to monitor database connection health using a configurable query. Adds a new constant `DEFAULT_QUERY` in `CnmmConfigurationOptions` for default health check queries. Updates `Program.cs` to register the new health check based on datasource type. Upgrades `PcAxis.Sql` package from version 1.4.3 to 1.5.0.
    
    * Update health check status and add unit tests
    
    Modified `CheckHealthAsync` to return `Degraded` status when a maintenance file is found. Added `MaintenanceHealthCheckTests` with tests for both healthy and degraded states, ensuring correct behavior of the health check under different conditions.
    
    * Refactor health check tests and improve error handling
    
    Renamed `MaintenanceHealthCheckTests` to `HealthCheckTests` and added a new test method `When_StrangeQuery_Then_DbCheckIsDown` to validate the behavior of the database health check with a non-existent table query.
    
    Updated the health check logic in `SqlDbConnectionHealthCheck.cs` to use a try-catch block for better error handling, ensuring that a healthy status is returned when the connection is good, and an appropriate unhealthy status is returned when it fails.
    
    * Refactor assertion order in HealthCheckTests
    
    Updated the parameter order in Assert.AreEqual calls in
    HealthCheckTests.cs to place the expected value first.
    This change enhances readability and follows common
    assertion practices.

---
+++ PxWeb.UnitTests/Helpers/HealthCheckTests.cs
@@ -0,0 +1,89 @@
+﻿using System.Threading;
+
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+
+using PxWeb.Code;
+
+namespace PxWeb.UnitTests.Helpers
+{
+
+    [TestClass]
+    public class HealthCheckTests
+    {
+
+        [TestMethod]
+        public void When_NoMaintenanceFile_Then_HealthCheckIsHealthy()
+        {
+            var host = new Mock<IPxHost>();
+            var tempPath = Path.GetTempPath();
+
+            host.Setup(h => h.RootPath).Returns(tempPath);
+
+            // Arrange
+            var healthCheck = new MaintenanceHealthCheck(host.Object);
+
+            // Act
+            var context = new HealthCheckContext();
+            var cancellationToken = CancellationToken.None;
+            var result = healthCheck.CheckHealthAsync(context, cancellationToken).Result;
+            // Assert
+            Assert.AreEqual(HealthStatus.Healthy, result.Status);
+        }
+
+
+        [TestMethod]
+        public void When_MaintenanceFile_Then_HealthCheckIsDegraded()
+        {
+            var host = new Mock<IPxHost>();
+            var tempPath = Path.GetTempPath();
+            var maintenanceFilePath = Path.Combine(tempPath, ".maintenance");
+
+            if (!File.Exists(maintenanceFilePath))
+            {
+                File.Create(maintenanceFilePath).Close();
+            }
+
+            host.Setup(h => h.RootPath).Returns(tempPath);
+
+            // Arrange
+            var healthCheck = new MaintenanceHealthCheck(host.Object);
+
+            // Act
+            var context = new HealthCheckContext();
+            var cancellationToken = CancellationToken.None;
+            var result = healthCheck.CheckHealthAsync(context, cancellationToken).Result;
+            // Assert
+            Assert.AreEqual(HealthStatus.Degraded, result.Status);
+
+            File.Delete(maintenanceFilePath);
+        }
+
+        [TestMethod]
+        public void When_StrangeQuery_Then_DbCheckIsDown()
+        {
+
+            // Arrange
+            var options = new Mock<IOptions<CnmmConfigurationOptions>>();
+
+            var config = new CnmmConfigurationOptions()
+            {
+                DatabaseID = "TestDatabase",
+                HealthCheckQuery = "SELECT * FROM NonExistentTable" // Intentionally wrong query
+            };
+
+            options.Setup(o => o.Value).Returns(config);
+
+            var healthCheck = new SqlDbConnectionHealthCheck(options.Object);
+
+            // Act
+            var context = new HealthCheckContext();
+            var cancellationToken = CancellationToken.None;
+            var result = healthCheck.CheckHealthAsync(context, cancellationToken).Result;
+            // Assert
+            Assert.AreEqual(HealthStatus.Unhealthy, result.Status);
+
+        }
+
+    }
+}
--- PxWeb/Config/Api2/CnmmConfigurationOptions.cs
+++ PxWeb/Config/Api2/CnmmConfigurationOptions.cs
@@ -2,6 +2,10 @@
 {
     public class CnmmConfigurationOptions
     {
+        public const string DEFAULT_QUERY = "select 1";
+
         public string DatabaseID { get; set; } = string.Empty;
+
+        public string HealthCheckQuery { get; set; } = DEFAULT_QUERY;
     }
 }
