7661d4a6aec1d37b42ce3628eaeea9d3b327d273
likp <petros.likidis@scb.se>
2025-10-23 13:12:50 +0200

    Added unit tests

---
+++ PxWeb.UnitTests/config/LuceneConfigurationServiceTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace PxWeb.UnitTests.config
+{
+    [TestClass]
+    public class LuceneConfigurationServiceTests
+    {
+        [TestMethod]
+        public void GetIndexDirectory_ReturnsCorrectPathFromRelativePath()
+        {
+            // Arrange
+            var hostPath = @"C:\inetpub\wwwwroot\api";
+            var hostMock = new Mock<IPxHost>();
+            hostMock.Setup(h => h.RootPath).Returns(hostPath);
+            var luceneConfig = new LuceneConfigurationOptions
+            {
+                IndexDirectory = @"Indexes\LuceneIndex"
+            };
+
+            var optionsMock = new Mock<IOptions<LuceneConfigurationOptions>>();
+            optionsMock.Setup(o => o.Value).Returns(luceneConfig);
+
+            var service = new LuceneConfigurationService(optionsMock.Object, hostMock.Object);
+
+            // Act
+            var indexDirectoryPath = service.GetIndexDirectoryPath();
+
+            // Assert
+            Assert.AreEqual(@"C:\inetpub\wwwwroot\api\Indexes\LuceneIndex", indexDirectoryPath);
+        }
+
+        [TestMethod]
+        public void GetIndexDirectory_ReturnsCorrectPathFromFullyQualifiedPath()
+        {
+            // Arrange
+            var hostPath = @"C:\inetpub\wwwwroot\api";
+            var hostMock = new Mock<IPxHost>();
+            hostMock.Setup(h => h.RootPath).Returns(hostPath);
+            var luceneConfig = new LuceneConfigurationOptions
+            {
+                IndexDirectory = @"\\apishare\Indexes\LuceneIndex"
+            };
+
+            var optionsMock = new Mock<IOptions<LuceneConfigurationOptions>>();
+            optionsMock.Setup(o => o.Value).Returns(luceneConfig);
+
+            var service = new LuceneConfigurationService(optionsMock.Object, hostMock.Object);
+
+            // Act
+            var indexDirectoryPath = service.GetIndexDirectoryPath();
+
+            // Assert
+            Assert.AreEqual(@"\\apishare\Indexes\LuceneIndex", indexDirectoryPath);
+        }
+    }
+}
