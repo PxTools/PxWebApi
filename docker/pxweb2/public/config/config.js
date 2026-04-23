3976ac5cd5548ad3bf8223fca2e167a505781953
Rune Johansen <runejo@gmail.com>
2026-04-23 20:41:51 +0200

    chore: update docker compose demo

--- docker/pxweb2/public/config/config.js
+++ docker/pxweb2/public/config/config.js
@@ -1,26 +1,28 @@
-window.PxWeb2Config = {
+globalThis.PxWeb2Config = {
   language: {
-    supportedLanguages: [{ shorthand: 'en', languageName: 'English' }],
-    defaultLanguage: 'en',
-    fallbackLanguage: 'en',
+    supportedLanguages: [{ shorthand: "en", languageName: "English" }],
+    defaultLanguage: "en",
+    fallbackLanguage: "en",
     showDefaultLanguageInPath: true,
+    positionInPath: "after",
   },
-  baseApplicationPath: '/',
-  apiUrl: '//localhost:8081/api/v2',
+  baseApplicationPath: "/",
+  apiUrl: "//localhost:8081/api/v2",
   maxDataCells: 150000,
+  useDynamicContentInTitle: false,
   showBreadCrumbOnStartPage: false,
-  specialCharacters: ['.', '..', ':', '-', '...', '*'],
+  specialCharacters: [".", "..", ":", "-", "...", "*"],
   variableFilterExclusionList: {
     en: [
-      'observations',
-      'year',
-      'quarter',
-      'month',
-      'every other year',
-      'every fifth year',
-    ]
+      "observations",
+      "year",
+      "quarter",
+      "month",
+      "every other year",
+      "every fifth year",
+    ],
   },
   homePage: {
-    en: '', // Set to your English homepage URL
+    en: "", // Set to your English homepage URL
   },
 };
