897f6ab9c4e42950a786be7dd2e378661669796d
likp <petros.likidis@scb.se>
2026-06-05 17:07:22 +0200

    Removed comments and other stuff

--- Px.Search.Lucene/LuceneAnalyzer.cs
+++ Px.Search.Lucene/LuceneAnalyzer.cs
@@ -66,41 +66,6 @@ namespace Px.Search.Lucene
 
         }
 
-
-        public const string SEARCH_FIELD_DOCID = "docid";
-        public const string SEARCH_FIELD_SEARCHID = "searchid";
-        public const string SEARCH_FIELD_UPDATED = "updated";
-        public const string SEARCH_FIELD_DISCONTINUED = "discontinued";
-        public const string SEARCH_FIELD_TAGS = "tags";
-        public const string SEARCH_FIELD_MATRIX = "matrix";
-        public const string SEARCH_FIELD_TITLE = "title";
-        public const string SEARCH_FIELD_DESCRIPTION = "description";
-        public const string SEARCH_FIELD_SORTCODE = "sortcode";
-        public const string SEARCH_FIELD_CATEGORY = "category";
-        public const string SEARCH_FIELD_FIRSTPERIOD = "firstperiod";
-        public const string SEARCH_FIELD_LASTPERIOD = "lastperiod";
-        public const string SEARCH_FIELD_VARIABLES = "variables";
-        public const string SEARCH_FIELD_PERIOD = "period";
-        public const string SEARCH_FIELD_VALUES = "values";
-        public const string SEARCH_FIELD_CODES = "codes";
-        public const string SEARCH_FIELD_GROUPINGS = "groupings";
-        public const string SEARCH_FIELD_GROUPINGCODES = "groupingcodes";
-        public const string SEARCH_FIELD_VALUESETS = "valuesets";
-        public const string SEARCH_FIELD_VALUESETCODES = "valuesetcodes";
-        public const string SEARCH_FIELD_SYNONYMS = "synonyms";
-        public const string SEARCH_FIELD_SOURCE = "source";
-        public const string SEARCH_FIELD_TIME_UNIT = "timeunit";
-        public const string SEARCH_FIELD_PATHS = "paths";
-        public const string SEARCH_FIELD_LEVEL_CODE = "levelcode";
-        public const string SEARCH_FIELD_LEVEL_NAME = "levelname";
-        public const string SEARCH_FIELD_META_ID = "metaid";
-        public const string SEARCH_FIELD_LEGACY_ID = "legacyid";
-        public const string SEARCH_SUBJECT_CODE = "subjectcode";
-        public const string SEARCH_AVAILABLE_LANGUAGES = "languages";
-
     }
-    // TODO ? Should read langs from config and prepare a static analyzerByLanguage dictionary? 
-
-    // depricated: Analyzer analyzer = new SnowballAnalyzer(LuceneVersion.LUCENE_48, "English");
 }
 
